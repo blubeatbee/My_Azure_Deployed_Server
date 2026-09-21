689f224f83d8887d0cd5ae458f5d947d81a5a843
Alex <alexcoh19@outlook.com>
2026-09-21 04:15:20 +0200

    feat!: main method will now use async

--- FullSack/Program.cs
+++ FullSack/Program.cs
@@ -12,7 +12,7 @@ namespace FullSack
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
